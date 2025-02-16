ca46b3bfda433a4b49af241a38dc3ccaaeb72c1a
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-02-15 22:06:01 -0600

    commit-cambios-archivos

--- functions/menu.java
+++ functions/menu.java
@@ -3,7 +3,7 @@ package functions;
 public class Menu{
 
     public void obtenerMenu(){
-        System.out.println("*** Menú de opciones ***");
+        System.out.println("*** Menu de opciones ***");
         System.out.println("1. Generar tabla de verdad");
         System.out.println("2. Salir");
     }
