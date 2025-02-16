6845982446f9b6083a1742a703768aade23738f7
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-02-15 21:27:53 -0600

    commit

---
+++ functions/menu.java
@@ -0,0 +1,10 @@
+
+package functions.menu.menu;
+
+public class menu{
+    public void menuTablas(){
+        System.out.println("*** Menú de opciones ***");
+        System.out.println("1. Generar tabla de verdad");
+        System.out.println("2. Salir");
+    }
+}
