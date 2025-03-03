50c3c27d961893a0c1a6973c22b8d005c6772bcf
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-03-02 23:27:04 -0600

    commit

--- front-tablas/src/components/table.tsx
+++ front-tablas/src/components/table.tsx
@@ -5,14 +5,14 @@ const AccionesTable = () => {
     return (
         <div className="Acciones-table row">
             <div className="boolean-binario d-flex justify-content-start align-items-center col-9 col-md-6">
-                <div className="d-flex justify-content-start align-items-center">
-                    <p>Boolean</p>
+                <div className="combina d-flex justify-content-start align-items-center">
+                    <p className='colorFull'>Boolean</p>
                     <ChangeData />
-                    <p>Binario</p>
+                    <p className='colorFull'>Binario</p>
                 </div>
             </div>
-            <div className="copiar-tabla col-3 col-md-6 d-flex justify-content-end align-items-center">
-                <button>
+            <div className="copiar-tabla  col-3 col-md-6 d-flex justify-content-end align-items-center" id='combina'>
+                <button className='combina'>
                     <i className="bi bi-copy"></i>
                     <span className="span-copy"></span>
                 </button>
@@ -29,15 +29,12 @@ const Table = () => {
         const fetchData = () => {
             const storedData = localStorage.getItem("tablas");
             const parsedData = storedData ? JSON.parse(storedData) : {};
-
-            // Obtener los encabezados de las claves del objeto
+            
             const newHeaders = Object.keys(parsedData);
             setHeaders(newHeaders);
 
-            // Obtener el número de filas
             const numRows = newHeaders.length > 0 && Array.isArray(parsedData[newHeaders[0]]) ? parsedData[newHeaders[0]].length : 0;
 
-            // Generar las filas de la tabla
             const newRows = [];
             for (let i = 0; i < numRows; i++) {
                 const cells = newHeaders.map((header, index) => (
