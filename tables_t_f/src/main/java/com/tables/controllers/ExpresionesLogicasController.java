0efcc49e628e2dad8410dcc3c9b771d0772ddf1b
Andrea17-cloud <andreajimenajeronimoa@gmail.com>
2025-03-02 18:36:14 -0600

    commit_operadoresLogicos

--- tables_t_f/src/main/java/com/tables/controllers/ExpresionesLogicasController.java
+++ tables_t_f/src/main/java/com/tables/controllers/ExpresionesLogicasController.java
@@ -6,22 +6,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
-
 public class ExpresionesLogicasController {
     @Data
-    public static class logicRequest{
+    public static class LogicRequest{
+        @JsonProperty("pProposicion")
         private Boolean pProposicion;
+        @JsonProperty("sProposicion")
         private Boolean sProposicion;
+        @JsonProperty("vLogico")
         private String vLogico;
     }
 
-    @PostMapping("/conjuncion")
-    public Boolean conjuncion(@RequestBody logicRequest request){
+    @PostMapping("/operadoresLogicos")
+    public Boolean operadoresLogicos(@RequestBody LogicRequest request){
         return request.getPProposicion() && request.getSProposicion();
     }
 }
